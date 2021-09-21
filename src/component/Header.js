d38dc8002bf4a5b7b8cf4bc6ede6bf5a2ebb8007
Maria Wright <maria@mariawrightstudios.com>
2021-09-20 22:04:50 -0400

    add link in header

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header className="header">
             <div className="header__wrapper">
                 <p className="header__app-name">Recipe <span className="header__app-name_accent">Saver</span></p>
-                <a className="header__link" href="#">About the Developer</a>
+                <a className="header__link" href="https://mentalcaries.github.io/code-jam-july-landing/index.html">About the Developer</a>
             </div>
         </header>
     )
